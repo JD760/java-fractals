d99e7366b51baf79f156518e848801e669b240f4
JD760 <jacobdicken2005@gmail.com>
2024-06-30 21:26:24 +0100

    Implemented a simple settings import/export system, allowing interesting locations to be saved and shared

--- src/menu/MenuBar.java
+++ src/menu/MenuBar.java
@@ -8,7 +8,8 @@ import javax.swing.AbstractAction;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import settings.Fractals;
 import settings.GlobalSettings;
 
 /**
@@ -27,6 +28,8 @@ public class MenuBar extends JMenuBar {
     private JMenuItem maxIterationsLabel = new JMenuItem();
     private JMenuItem fractalTypeLabel = new JMenuItem();
     private JMenuItem copySettings = new JMenuItem("Copy Settings");
+    private JTextField importSettings = new JTextField();
+    private JMenuItem seedLabel = new JMenuItem();
 
     public InfoMenu(String text) {
       super(text);
@@ -34,17 +37,23 @@ public class MenuBar extends JMenuBar {
       add(scaleLabel);
       add(maxIterationsLabel);
       add(fractalTypeLabel);
+      add(seedLabel);
       addSeparator();
       add(copySettings);
+      importSettings.addActionListener(new ImportAction());
+      add(importSettings);
     }
 
     public void refresh() {
       centerLabel.setText("Center: " + settings.center.toString());
       scaleLabel.setText("Scale: " + Double.toString(settings.scale));
       maxIterationsLabel.setText("Max Iterations: " + settings.maxIterations);
-      fractalTypeLabel.setText("Mode: " + settings.mode.toString());
+      fractalTypeLabel.setText("Mode: " + Fractals.toString(settings.mode));
+      seedLabel.setText("Seed: " + settings.seed.toString());
       copySettings.setAction(new CopyAction());
       copySettings.setText("Copy Settings");
+      seedLabel.setVisible(
+          settings.mode == Fractals.JULIA || settings.mode == Fractals.JULIA_DIVERGENCE);
     }
 
     class CopyAction extends AbstractAction {
@@ -61,6 +70,61 @@ public class MenuBar extends JMenuBar {
         clipboard.setContents(new StringSelection(settingsStr.toString()), null);
       }
     }
+    
+    class ImportAction extends AbstractAction {
+      String text = "test";
+
+      /**
+       * Attempt to parse the settings string and apply the relevant changes if the string is valid
+       * otherwise stop the parsing and inform the user of the parsing issue.
+       */
+      public void actionPerformed(ActionEvent e) {
+        JTextField source = (JTextField) e.getSource();
+        String text = source.getText();
+        if (text.charAt(0) != '{' || text.charAt(text.length() - 1) != '}') {
+          invalidSettings(source, "Invalid format...");
+          return;
+        }
+        text = text.substring(1, text.length());
+        String[] settingsStr = text.split(",");
+        if (settingsStr.length != 5) {
+          invalidSettings(source, "Invalid number of params...");
+          return;
+        }
+
+        try {
+          settings.center.setRe(Double.parseDouble(settingsStr[0]));
+          settings.center.setIm(Double.parseDouble(settingsStr[1]));
+          settings.scale = Double.parseDouble(settingsStr[2]);
+          settings.maxIterations = Integer.parseInt(settingsStr[3]);
+        } catch (NumberFormatException nfe) {
+          System.out.println(nfe);
+          invalidSettings(source, "Invalid number format...");
+          return;
+        }
+
+        settings.mode = Fractals.getElement(settingsStr[4]);
+        if (settings.mode == null) {
+          settings.mode = Fractals.MANDELBROT;
+        }
+
+        settings.panel.repaint();
+      }
+
+      private void invalidSettings(JTextField source, String message) {
+        source.setText(message);
+        source.selectAll();
+      }
+    }
+  }
+
+  class FileMenu extends JMenu {
+    private JMenuItem saveImage = new JMenuItem("Save Image");
+
+    public FileMenu(String name) {
+      super(name);
+      add(saveImage);
+    }
   }
 
   /**
@@ -71,15 +135,9 @@ public class MenuBar extends JMenuBar {
   public MenuBar(GlobalSettings settings) {
     this.settings = settings;
 
-    fileMenu = new JMenu("File");
+    fileMenu = new FileMenu("File");
     modeMenu = new JMenu("Mode");
     infoMenu = new InfoMenu("Info");
-
-    JMenu infoSubmenu = new JMenu("Import");
-    infoSubmenu.add(new JTextArea());
-    infoSubmenu.add(new JMenuItem("Set"));
-    //infoMenu.add(infoSubmenu);
-
 
     add(fileMenu);
     add(modeMenu);
--- src/settings/Fractals.java
+++ src/settings/Fractals.java
@@ -7,5 +7,48 @@ public enum Fractals {
   MANDELBROT,
   DIVERGENCE_SCHEME,
   JULIA,
-  JULIA_DIVERGENCE
+  JULIA_DIVERGENCE;
+
+  /**
+   * Return a detailed string for each enumeration item that is more user-friendly
+   * for use in the GUI.
+
+   * @param mode - the item to convert to its' localised name
+   * @return - the localised name representing the item mode
+   */
+  public static String toString(Fractals mode) {
+    switch (mode) {
+      case MANDELBROT:
+        return "Mandelbrot Set";
+      case DIVERGENCE_SCHEME:
+        return "Mandelbrot Set (divergence scheme)";
+      case JULIA:
+        return "Julia Set";
+      case JULIA_DIVERGENCE:
+        return "Julia Set (divergence scheme)";
+      default:
+        return "";
+    }
+  }
+
+  /**
+   * Return the enum element corresponding to the string mode, else return null.
+
+   * @param mode - a string that may or may not correspond to an enum item
+   * @return - the enum item if there exists one matching the string mode, else null
+   */
+  public static Fractals getElement(String mode) {
+    switch (mode) {
+      case "MANDELBROT":
+        return Fractals.MANDELBROT;
+      case "DIVERGENCE_SCHEME":
+        return Fractals.DIVERGENCE_SCHEME;
+      case "JULIA":
+        return Fractals.JULIA;
+      case "JULIA_DIVERGENDE":
+        return Fractals.JULIA_DIVERGENCE;
+      default:
+        return null;
+    }
+  }
 }
